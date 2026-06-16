3ead379309bb0a362cc4d9aacdea7ed4f1e941ba
Evelien <edewals2009@gmail.com>
2026-06-16 12:02:32 +0200

    added rat manager and implemented rat visitors to add rats to the colony

---
+++ Assets/Scripts/RatManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+
+public class RatManager : MonoBehaviour
+{
+    [SerializeField] private float babyMakingCooldown;
+    [SerializeField] private float ratVisitorCooldown;
+    [SerializeField] private GameObject ratPrefab;
+    [Tooltip("spawnPosition should be just outside of screen so it seems the rat walks to colony")]
+    [SerializeField] private Transform spawnPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(RatVisitor());
+    }
+
+    IEnumerator RatVisitor()
+    {
+        yield return new WaitForSeconds(ratVisitorCooldown);
+
+        Instantiate(ratPrefab, spawnPosition.position, transform.rotation);
+        StartCoroutine(RatVisitor());
+    }
+
+}
