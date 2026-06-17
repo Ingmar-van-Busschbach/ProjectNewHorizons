0022b8cc2fccb831c6cdcb6aa3354f1e9dbf6e3b
Ingmar van Busschbach <ingmarmatthis@gmail.com>
2026-06-17 14:37:37 +0200

    Fixed animations not angling the character by overhauling the animation and rotation system.

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -1,10 +1,13 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class Character : MonoBehaviour
 {
-    public Room currentRoom;
+    [SerializeField] private float idleRotationSpeed = 5;
+    // Components
+    [HideInInspector] public Room currentRoom;
     private NavMeshAgent navMeshAgent;
     private void Start()
     {
@@ -14,5 +17,31 @@ public class Character : MonoBehaviour
     public void MoveToLocation(Transform location)
     {
         navMeshAgent.SetDestination(location.position);
+    }
+
+    private void Update()
+    {
+        if (!navMeshAgent.pathPending)
+        {
+            // Check if the remaining distance is within the stopping distance
+            if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+            {
+                // Verify the agent actually has a path and isn't just stopped
+                if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0f)
+                {
+                    StartCoroutine(OnArrival());
+                }
+            }
+        }
+    }
+
+    private IEnumerator OnArrival()
+    {
+        Quaternion targetRotation = Vector3.Dot(transform.right, Vector3.right) < 0 ? Quaternion.Euler(0, 90, 0) : Quaternion.Euler(0, -90, 0);
+        while (Quaternion.Angle(transform.rotation, targetRotation) > 5)
+        {
+            yield return new WaitForFixedUpdate();
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, idleRotationSpeed * Time.fixedDeltaTime);
+        }
     }
 }
