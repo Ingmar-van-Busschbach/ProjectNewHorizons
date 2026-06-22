df0a309c3a51673f28b40c7c04d2506ab164e42c
Evelien <edewals2009@gmail.com>
2026-06-22 11:42:55 +0200

    added plague spread & edited test scene to test

---
+++ Assets/Scripts/Rooms/PlagueSpreadRoom.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class PlagueSpreadRoom : Room
+{
+    [Header("Leave Handler")]
+    [Tooltip("amount the plague goes up per rat send out")]
+    [SerializeField] private float plagueSpread;
+    [Tooltip("min amount it takes for a rat to return")]
+    [SerializeField] private float minReturnTime;
+    [Tooltip("max amount it takes for a rat to return")]
+    [SerializeField] private float maxReturnTime;
+    [Tooltip("minimal amount of plague spread per rat per time")]
+    [SerializeField] private int minPlague = 5;
+    [Tooltip("max amount of plague spread per rat per time")]
+    [SerializeField] private int maxPlague = 10;
+
+    Character ratThatLeaves;
+
+
+    Transform leaveLocation;
+
+    public override Transform AssignCharacter(Character character)
+    {
+        if (unlockedRoom)
+        {
+            for (int i = 0; i < characterLocations.Count; i++)
+            {
+                if (!characterIndex.Values.Contains(i))
+                {
+                    characterIndex.Add(character, i);
+                    character.currentRoom = this;
+                    StartCoroutine(ratLeave());
+                    return characterLocations[i];
+                }
+            }
+        }
+        return character.gameObject.transform;
+    }
+    IEnumerator ratLeave()
+    {
+        //if rat is infected
+        ratThatLeaves.MoveToLocation(leaveLocation);
+        int plagueAmount = Random.Range(minPlague, maxPlague);
+        float returnTime = Random.Range(minReturnTime, maxReturnTime);
+        yield return new WaitForSeconds(returnTime);
+
+        ResourceManager.instance.ResourceHandler(EResourceType.Plague, plagueAmount);
+
+        for (int i = 0; i < characterLocations.Count; i++)
+        {
+            if (!characterIndex.Values.Contains(i))
+            {
+                ratThatLeaves.MoveToLocation(characterLocations[i]);
+            }
+        }
+
+    }
+}
